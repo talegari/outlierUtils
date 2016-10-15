8d5022ff7c00211b018014130ad9828819e4c004
talegari <sri.teach@gmail.com>
2016-10-15 18:36:04 +0530

    is outlier version for data frame

---
+++ R/is_outlier.dataframe.R
@@ -0,0 +1,38 @@
+###############################################################################
+# is_outlier.dataframe
+###############################################################################
+#
+# function returns a logical vector indicating whether a row is an outlier
+#
+# Procedure: Each column is normalized to contain values between -1 and 1.
+# For each row, euclidean distance(normalized by dividing by length of the row)
+# between the row and the column medians is computed and passed to `is_outlier`
+#
+###############################################################################
+#
+# author  : Srikanth KS (talegari)
+# license : GNU AGPLv3 (http://choosealicense.com/licenses/agpl-3.0/)
+#
+###############################################################################
+
+is_outlier.dataframe <- function(x, coef = 1.5){
+
+  stopifnot(is.data.frame(x))
+  stopifnot(unique(vapply(x, class, character(1)) %in% c("numeric", "integer")))
+
+  euc_dist_normalized <- function(x, y){
+    pos <- (!is.na(x)) & (!is.na(y))
+    return( sqrt(sum((x[pos] - y[pos])^2))/sum(pos) )
+    }
+
+  normalize1 <- function(mat){
+    apply(x, 2, function(acol){ acol/max(abs(acol)) })
+  }
+
+  y <-  normalize1(x)
+
+  colMedian <- apply(y, 2, function(acol2){median(acol2, na.rm = TRUE)})
+
+  pointEst  <- apply(x, 1, function(arow){euc_dist_normalized(arow, colMedian)})
+  return( is_outlier(pointEst) )
+}
