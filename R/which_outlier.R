4ef718f790c50ddb26fdfaea3a008d8a52f1485e
talegari <sri.teach@gmail.com>
2016-10-26 08:08:17 +0530

    with new name

---
+++ R/which_outlier.R
@@ -0,0 +1,28 @@
+###############################################################################
+# which_outlier
+###############################################################################
+#
+# function returns positions of the outliers
+# NA's are not considered as outliers
+# the logic is similar to `boxplot.stats`
+#
+###############################################################################
+#
+# author  : Srikanth KS (talegari)
+# license : GNU AGPLv3 (http://choosealicense.com/licenses/agpl-3.0/)
+#
+###############################################################################v
+
+which_outlier <- function(x, coef = 1.5){
+
+  stopifnot(class(x) %in% c("numeric", "integer"))
+  stopifnot(coef >= 0)
+  stats <- stats::fivenum(x, na.rm = TRUE)
+  iqr   <- diff(stats[c(2L, 4L)])
+  out   <- x < (stats[2L] - (coef * iqr)) | x > (stats[4L] + (coef * iqr))
+  if(sum(out) != 0){
+    return(which(out))
+  } else {
+    return(integer())
+  }
+}
